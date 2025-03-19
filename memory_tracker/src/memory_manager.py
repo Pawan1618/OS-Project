dcebbb664b8b685c44ae50a7278feb83f18f3c19
pawan <pawansinghshiva2030@gmail.com>
2025-03-19 21:29:29 +0530

    Added base MemoryManager class

---
+++ memory_tracker/src/memory_manager.py
@@ -0,0 +1,10 @@
+class MemoryManager:
+    def __init__(self, total_memory):
+        self.total_memory = total_memory
+        self.used_memory = 0
+
+    def allocate_memory(self, process):
+        raise NotImplementedError
+
+    def free_memory(self, process):
+        raise NotImplementedError
