09f4f5413dbe53d4d147329f24db1e2677aa7670
pawan <pawansinghshiva2030@gmail.com>
2025-03-20 18:01:40 +0530

    Final integration and optimizations

---
+++ memory_tracker/main.py
@@ -0,0 +1,10 @@
+from src.paging import PagingMemoryManager
+from src.segmentation import SegmentationMemoryManager
+from src.process import Process
+
+if __name__ == "__main__":
+    mem_manager = PagingMemoryManager(total_memory=1024, page_size=64)
+
+    p1 = Process(1, 128)
+    mem_manager.allocate_memory(p1)
+    print("Memory allocated:", mem_manager.pages)
