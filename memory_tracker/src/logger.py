35c68040bcb191f01742dc9988a1f72ad7237f6a
Shivansh <003shivansh@gmail.com>
2025-03-21 14:51:46 +0530

    delete

--- memory_tracker/src/logger.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def log_memory_action(action, process):
-    with open("../logs/memory_log.txt", "a") as file:
-        file.write(f"{datetime.datetime.now()} - {action}: {process}\n")     
