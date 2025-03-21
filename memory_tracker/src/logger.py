79054dc8c696cd9c47ddb5fb1ed097e49c7625b9
Shivansh <003shivansh@gmail.com>
2025-03-21 14:47:20 +0530

    new

--- memory_tracker/src/logger.py
+++ memory_tracker/src/logger.py
@@ -3,3 +3,4 @@ import datetime
 def log_memory_action(action, process):
     with open("../logs/memory_log.txt", "a") as file:
         file.write(f"{datetime.datetime.now()} - {action}: {process}\n")
+        
