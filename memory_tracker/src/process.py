2d3deff9f025556d1a4fb05e9d5ca932c663d1fb
pawan <pawansinghshiva2030@gmail.com>
2025-03-21 19:26:48 +0530

    better process file

--- memory_tracker/src/process.py
+++ memory_tracker/src/process.py
@@ -1,11 +1,27 @@
 class Process:
-    def __init__(self, pid, size):
-        self.pid = pid
-        self.size = size
-        self.status = "Running"
+    def __init__(self, process_id, name, memory_size):
+        self.process_id = process_id
+        self.name = name
+        self.memory_size = memory_size
+        self.state = "NEW"  # States: NEW, RUNNING, WAITING, TERMINATED
+        self.allocation_time = None
+        self.segments = []  # List of memory segments allocated to this process
 
-    def terminate(self):
-        self.status = "Terminated"
+    def update_state(self, new_state):
+        """Update process state"""
+        self.state = new_state
 
-    def __repr__(self):
-        return f"Process({self.pid}, Size={self.size}, Status={self.status})"
+    def add_segment(self, segment):
+        """Add a memory segment to the process"""
+        self.segments.append(segment)
+
+    def clear_segments(self):
+        """Clear all memory segments"""
+        self.segments.clear()
+
+    def get_total_memory(self):
+        """Get total memory allocated to the process"""
+        return sum(segment['size'] for segment in self.segments)
+
+    def __str__(self):
+        return f"Process(ID={self.process_id}, Name={self.name}, Size={self.memory_size}, State={self.state})"
